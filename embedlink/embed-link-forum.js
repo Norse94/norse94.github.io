840403a9db594d9ba77ba9ac5d43a9b1809c4274
Norse94 <trekkyp36@gmail.com>
2026-07-04 23:03:21 +0200

    NEUMAN

--- embedlink/embed-link-forum.js
+++ embedlink/embed-link-forum.js
@@ -1,10 +1,10 @@
-/* FD EMBED LINK build 2026-07-04.32 */
+/* FD EMBED LINK build 2026-07-04.33 */
 (() => {
   "use strict";
 
   const CONFIG = {
     appTitle: "FD EMBED LINK",
-    version: "2026-07-04.32",
+    version: "2026-07-04.33",
     edgeEndpoint: "https://mycvmmlezpxdoamecrhb.functions.supabase.co/embed-link",
     allowedForumHosts: ["difesa.forumfree.it", "difesaitalia.forumfree.it"],
     maxImages: 5,
