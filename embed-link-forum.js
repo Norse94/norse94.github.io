bfb2e6c73cce9ccf8ce51b3c96a2349a4fdf6902
Norse94 <trekkyp36@gmail.com>
2026-07-04 16:18:49 +0200

    Update embed-link-forum.js

--- embed-link-forum.js
+++ embed-link-forum.js
@@ -1,10 +1,10 @@
-/* FD EMBED LINK build 2026-07-04.17 */
+/* FD EMBED LINK build 2026-07-04.18 */
 (() => {
   "use strict";
 
   const CONFIG = {
     appTitle: "FD EMBED LINK",
-    version: "2026-07-04.17",
+    version: "2026-07-04.18",
     edgeEndpoint: "https://mycvmmlezpxdoamecrhb.functions.supabase.co/embed-link",
     allowedForumHosts: ["difesa.forumfree.it", "difesaitalia.forumfree.it"],
     maxImages: 5,
@@ -1152,8 +1152,7 @@
       title: APP_TITLE,
       event: async () => {
         await openUrlModal("");
-      },
-      allowCustomEditors: false
+      }
     };
 
     if (!state.classicButtonRegistered && buttons && typeof buttons.add === "function") {
