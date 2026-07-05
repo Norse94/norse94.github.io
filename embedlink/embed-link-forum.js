ce146929787ad44fa8a6aace23c816c4e214e62a
Norse94 <trekkyp36@gmail.com>
2026-07-05 03:51:41 +0200

    ZIGZIGMAN

--- embedlink/embed-link-forum.js
+++ embedlink/embed-link-forum.js
@@ -1,10 +1,10 @@
-/* FD EMBED LINK build 2026-07-05.16 */
+/* FD EMBED LINK build 2026-07-05.17 */
 (() => {
   "use strict";
 
   const CONFIG = {
     appTitle: "FD EMBED LINK",
-    version: "2026-07-05.16",
+    version: "2026-07-05.17",
     edgeEndpoint: "https://mycvmmlezpxdoamecrhb.functions.supabase.co/embed-link",
     allowedForumHosts: ["difesa.forumfree.it", "difesaitalia.forumfree.it"],
     maxImages: 5,
